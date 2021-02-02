8d2e56440438d940bf8878f44fc13b4694732b81
Josué Heringer <josuedelgadoheringer98@gmail.com>
2021-02-01 22:32:42 -0300

    finalizando tela de login

--- sgira/resources/views/auth/login.blade.php
+++ sgira/resources/views/auth/login.blade.php
@@ -38,21 +38,7 @@
                         </span>
                     @endif
                 </div>
-                <div class="row">
-                    <div class="col-8">
-                        <div class="form-check">
-                            <input class="form-check-input" type="checkbox" name="remember" id="remember"
-                                {{ old('remember') ? 'checked' : '' }}>
-
-                            <label class="form-check-label" for="remember">
-                                Lembre-me
-                            </label>
-                        </div>
-                    </div>
-                    <div class="col-4">
-                        <button type="submit" class="btn btn-dark btn-block">Login</button>
-                    </div>
-                </div>
+                <button type="submit" class="btn btn-dark btn-block">Login</button>
             </form>
         </div>
     </div>
