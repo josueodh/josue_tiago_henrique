beb010e8c39b5781bae93633d59bad66cc2552bc
Henrique <henriqueaureliocsilva@gmail.com>
2021-02-02 22:37:15 -0300

    factory img

--- sgira/database/factories/PartnerFactory.php
+++ sgira/database/factories/PartnerFactory.php
@@ -7,10 +7,10 @@ use Faker\Generator as Faker;
 
         
 $factory->define(Partner::class, function (Faker $faker) {
-    if (!\File::exists(public_path('img\partners'))) {
-        \File::makeDirectory(public_path('img\partners'));  //follow the declaration to see the complete signature
+    if (!\File::exists(storage_path('img\partners'))) {
+        \File::makeDirectory(storage_path('img\partners'));  //follow the declaration to see the complete signature
     }
-    $filepath = $faker->image(public_path('img\partners'));
+    $filepath = $faker->image(storage_path('img\partners'));
     $image = array_values(array_slice(explode('/', $filepath), -1))[0];
     $name= $faker->unique()->name;
     dd($filepath);
